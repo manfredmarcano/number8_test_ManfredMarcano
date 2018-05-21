84a4e0ccfe8d53334f8598abb91c24db5ef00423
Manfred Marcano <manfredalberto@gmail.com>
2018-05-21 17:53:42 -0400

    Inputs creation/CSS

--- js/calendar.js
+++ js/calendar.js
@@ -1,14 +1,14 @@
 var months = [
-	{ name: "January", 	length: 31 },
+	{ name: "January", length: 31 },
 	{ name: "February", length: 28 },
-	{ name: "March", 	length: 31 },
-	{ name: "April", 	length: 30 },
-	{ name: "May", 		length: 31 },
-	{ name: "June", 	length: 30 },
-	{ name: "July", 	length: 31 },
-	{ name: "August", 	length: 31 },
-	{ name: "September",length: 30 },
-	{ name: "October", 	length: 31 },
+	{ name: "March", length: 31 },
+	{ name: "April", length: 30 },
+	{ name: "May", length: 31 },
+	{ name: "June", length: 30 },
+	{ name: "July", length: 31 },
+	{ name: "August", length: 31 },
+	{ name: "September", length: 30 },
+	{ name: "October", length: 31 },
 	{ name: "November", length: 30 },
 	{ name: "December", length: 31 }
 ];
